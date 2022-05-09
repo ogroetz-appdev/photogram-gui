6e2c45cc08f1637fec78ce7cd11cb7ef536c4fdd
osha7 <ogroetz@gmail.com>
2022-05-09 01:17:33 +0000

    photo detail info and comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,17 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
 
-    render({ :template => "photo_templates/photos.html.erb"})
+    render({ :template => "photo_templates/photos.html.erb" })
+  end
+
+  def show_detail
+    @photo_id = params.fetch("id")
+    @photo = Photo.where({ :id => @photo_id }).at(0)
+
+    if (!@photo)
+      redirect_to("/404")
+    else
+      render({ :template => "/photo_templates/photo_detail.html.erb" })
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get('/users/:name', {:controller => "users", :action => "show_detail"})
 
   get('/photos', {:controller => "photos", :action => "index"})
+  get('/photos/:id', {:controller => "photos", :action => "show_detail"})
 
 end
